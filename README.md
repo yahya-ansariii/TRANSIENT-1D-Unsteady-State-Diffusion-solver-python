e4f2bb83e5908850689677206ddd7a13041f2afd
Yahya <yahyaansari62@gmail.com>
2021-06-20 21:04:14 +0530

    add readme,notes + use function + export result

---
+++ README.md
@@ -0,0 +1,47 @@
+# TRANSIENT-1D-Unsteady State Diffusion solver
+## Python code for Transient 1 Dimension Unsteady State Heat Conduction
+
+Read _[Transient.pdf](Transient.pdf)_ in the repository to understand the Transient 1D unsteady state diffusion.
+The _governing equation_ used in the python code is
+
+![governing equation](images/ge.PNG)
+
+The python code has all the necessary comments to understand the solver.
+
+_**Note** : In this solver cross sectional area is not required as we deal in one dimension only, so we measure unsteady heat conduction across the length only._
+
+
+## Motivation
+To learn Python programming and use python packages.
+
+## Dependencies
+- [Python](https://python.org) 3.9.5
+
+- [Pandas](https://pandas.pydata.org) 1.2.4
+
+- [Openpyxl](https://openpyxl.readthedocs.io) 3.0.7
+
+
+## Output
+User input data :
+
+![input](images/1.PNG)
+
+Solution which is printed in tabular form :
+
+![table](images/2.PNG)
+
+
+Export result to output folder :
+
+_**Note** : Previous saved result will be overwritten so copy the previous result to some other folder._
+
+![export](images/4.PNG)
+
+## License
+
+[Apache License 2.0](LICENSE)
+
+## Author
+
+[Yahya Ansari](https://github.com/novus-afk)
--- transient.py
+++ transient.py
@@ -13,25 +13,26 @@
    limitations under the License.
 '''
 import pandas as pd
-import numpy as np
 
-
+# take user input
 print("\n \t\t TRANSIENT Solver\n")
-n = int(input("\n\tEnter the no. of grid points:   "))
+print("""\n\tTo learn more about Transient heat conduction read the Transient.pdf
+    """)
+n = int(input("\n\tEnter the no. of grid points :   "))
 
-l = float(input("\n\tEnter length of plate in m:   "))
+l = float(input("\n\tEnter length of plate in m :   "))
 
-tk = float(input("\n\tEnter thermal conductivity in W/mK or W/mC:   "))
+tk = float(input("\n\tEnter thermal conductivity in W/mK or W/mC :   "))
 
-rc = float(input("\n\tEnter rhoC in J/m3K:   "))
+rc = float(input("\n\tEnter rhoC in J/m3K :   "))
 
-ti = float(input("\n\tEnter Initial temperature at T = 0 sec:   "))
+ti = float(input("\n\tEnter initial temperature at T = 0 sec :   "))
 
-te = float(input("\n\tEnter temperature at east side of plate:   "))
+te = float(input("\n\tEnter temperature at east side of plate :   "))
 
-dt = float(input("\n\tEnter dt:   "))
+dt = float(input("\n\tEnter dt :   "))
 
-step = int(input("\n\tEnter number of time step:   "))
+step = int(input("\n\tEnter number of time step :   "))
 
 
 # create empty list
@@ -42,20 +43,27 @@ c = [0]*n
 A = [0]*n
 C = [0]*n
 X = [0]*n
+
 temp = [0]*n
 tempold = [0]*n
+
+# create dataframe to store result in table
+df = pd.DataFrame(columns=["Node: 1", "Node: 2",
+                  "Node: 3", "Node: 4", "Node: 5"])
 
 dx = l/n
 Z = (rc*dx)/dt
 z = tk/dx
 
-#TDMA function very specific to this numerical solver, for general tdma solver refer : https://github.com/novus-afk/TDMA-Solver
+# Define TDMA function very specific to this numerical solver, for general tdma solver refer : https://github.com/novus-afk/TDMA-Solver
+
+
 def TDMA(n, beta, D, alpha, c):
     beta[0] = 0
     beta[n-1] = beta[1]
     alpha[0] = alpha[1]
     alpha[n-1] = 0
-    #copy common values
+    # copy common values
     for i in range(1, n-1):
         D[i] = D[1]
         beta[i] = beta[1]
@@ -75,8 +83,86 @@ def TDMA(n, beta, D, alpha, c):
     return X
 
 
-# create dataframe to store result in table
-df = pd.DataFrame(columns=["1", "2", "3", "4", "5"])
+def EXPLICIT(temp, tempold):
+    for o in range(0, n):
+        temp[o] = ti
+        tempold[o] = ti
+
+    for i in range(0, step+1):
+        print("Temperature values for time step {} are:\n" .format(i))
+        for k in range(0, n):
+            print(" T%d = %f C\t" % (k+1, temp[k]))
+        print("\n")
+        df.loc[len(df.index)] = tempold  # add data to dataframe last row
+        temp[0] = (((Z-z)*tempold[0])+(z*tempold[1]))/Z
+
+        for j in range(1, n-1):
+            temp[j] = (((Z-(2*z))*tempold[j]) +
+                       (z*tempold[j-1])+(z*tempold[j+1]))/Z
+
+        temp[n-1] = (((Z-(3*z))*tempold[n-1]) + (z*tempold[n-2]) + (2*te*z))/Z
+        tempold = temp.copy()
+
+
+# noinspection PyUnresolvedReferences
+def IMPLICIT(temp, tempold):
+    # initialize data for tdma function
+    D[0] = Z+z
+    D[1] = Z+z+z
+    D[n-1] = Z+z+z+z
+    beta[1] = z
+    alpha[1] = z
+    # copy initial temperature
+    for o in range(0, n):
+        temp[o] = ti
+    tempold = temp.copy()
+
+    for p in range(0, step+1):
+        print("Temperature values for time step {} are:\n" .format(p))
+        # print temp values
+        for i in range(0, n):
+            print("T%d = %f C \t" % (i+1, temp[i]))
+        print("\n")
+
+        # add new temp values to the pandas dataframe(add new temperature row, similar to printing values at each time step)
+        df.loc[len(df.index)] = temp
+
+        for i in range(0, n):
+            c[i] = Z*tempold[i]
+        temp = TDMA(n, beta, D, alpha, c)
+        tempold = temp.copy()
+
+
+def CrankN(temp, tempold):
+    D[0] = Z+(z/2)
+    D[1] = Z+z
+    D[n-1] = Z+z
+    beta[1] = z/2
+    alpha[1] = z/2
+
+    for o in range(0, n):
+        temp[o] = ti
+    tempold = temp.copy()
+
+    print("Temperature values are:\n")
+
+    for p in range(0, step+1):
+        print("Temperature values for time step {} are:\n" .format(p))
+        # print temp values
+        for i in range(0, n):
+            print("T%d = %f C \t" % (i+1, temp[i]))
+        print("\n")
+        df.loc[len(df.index)] = temp
+
+        # set constant values
+        c[0] = (Z * tempold[0]) + (z * 0.5 * (tempold[1] - tempold[0]))
+        c[n-1] = ((Z-z)*tempold[n-1]) + (z * 0.5 * tempold[n-2])
+        for i in range(1, n-1):
+            c[i] = ((Z-z)*tempold[i]) + (z * 0.5 * (tempold[i-1]+tempold[i+1]))
+
+        temp = TDMA(n, beta, D, alpha, c)
+        tempold = temp.copy()
+
 
 # Switch case for type of numerical
 choice = ""
@@ -94,88 +180,18 @@ while choice != "q":
 
     if choice == "1":
         print("\n\tExplicit Scheme\n")
-        for o in range(0, n):
-            temp[o] = ti
-            tempold[o] = ti
-
-        for i in range(0, step+1):
-            print("Temperature values for time step {} are:\n" .format(i))
-            for k in range(0, n):
-                print(" T%d = %f C\t" % (k+1, temp[k]))
-            print("\n")
-            df.loc[len(df.index)] = tempold
-            temp[0] = (((Z-z)*tempold[0])+(z*tempold[1]))/Z
-
-            for j in range(1, n-1):
-                temp[j] = (((Z-(2*z))*tempold[j]) +
-                           (z*tempold[j-1])+(z*tempold[j+1]))/Z
-
-            temp[n-1] = (((Z-(3*z))*tempold[n-1]) +
-                         (z*tempold[n-2]) + (2*te*z))/Z
-            tempold = temp.copy()
+        EXPLICIT(temp, tempold)
         break
-
 
     elif choice == "2":
         print("\n\tImplicit Scheme\n")
-        #initialize data for tdma function
-        D[0] = Z+z
-        D[1] = Z+z+z
-        D[n-1] = Z+z+z+z
-        beta[1] = z
-        alpha[1] = z
-        #copy initial temperature
-        for o in range(0, n):
-            temp[o] = ti
-        tempold = temp.copy()
-
-        print("Temperature values are:\n")
-
-        #print temp values
-        for p in range(0, step+1):
-            for i in range(0, n):
-                print("T%d = %f C \t" % (i+1, temp[i]))
-            print("\n")
-            
-            #add new temp values to the pandas dataframe(add new temperature row, similar to printing values at each time step)
-            df.loc[len(df.index)] = temp
-
-            for i in range(0, n):
-                c[i] = Z*tempold[i]
-
-            temp = TDMA(n, beta, D, alpha, c)
-            tempold = temp.copy()
+        IMPLICIT(temp, tempold)
         break
 
-    #solve for crank nicolson scheme (currently it is a copy of implicit)
+    # solve for crank nicolson scheme (currently it is a copy of implicit)
     elif choice == "3":
         print("\n\tCrank Nicolson Scheme\n")
-        D[0] = Z+(z/2)
-        D[1] = Z+z
-        D[n-1] = Z+z
-        beta[1] = z/2
-        alpha[1] = z/2
-        
-        for o in range(0, n):
-            temp[o] = ti
-        tempold = temp.copy()
-
-        print("Temperature values are:\n")
-
-        for p in range(0, step+1):
-            for i in range(0, n):
-                print("T%d = %f C \t" % (i+1, temp[i]))
-            print("\n")
-            df.loc[len(df.index)] = temp
-
-            #set constant values
-            c[0] = (Z * tempold[0]) + (z * 0.5 * (tempold[1] - tempold[0]))
-            c[n-1] = ((Z-z)*tempold[n-1]) + (z * 0.5 * tempold[n-2])
-            for i in range(1, n-1):
-                c[i] = ((Z-z)*tempold[i]) + (z * 0.5 * (tempold[i-1]+tempold[i+1]))
-
-            temp = TDMA(n, beta, D, alpha, c)
-            tempold = temp.copy()
+        CrankN(temp, tempold)
         break
 
     elif choice == "q":
@@ -185,6 +201,36 @@ while choice != "q":
         print("\n\n\tInvalid choice, Try again!\n")
 
 
+# add time column
+Ts = []
+for i in range(0, step+1):
+    Ts.append(i*dt)
+df.insert(0, "Time(sec)", Ts)
+
+# print DataFrame
 print(df)
 
-input("Press enter to exit")
+
+# save choice
+while choice != "q":
+    print('''\n\t[ y ] Save result to excel file.
+    
+        [ q ] Exit without saving result
+        ''')
+    choice = input("\nEnter yout choice :\t")
+    if choice == "y":
+        # add time step column at the start of the DataFrame
+        df.insert(0, 'Time Step.', range(0, len(df)))
+        df.to_excel("output//Transient.xlsx", sheet_name='Output',
+                    index=False)  # .to_excel to export excel file
+        print(
+            "\n\n*************** Export complete! Check output folder. ***************\n\n")
+        break
+    elif choice == "q":
+        print("\n***** Result not saved *****")
+        break
+    else:
+        print("\n Invalid Choice, Try again !")
+
+
+input("Press Enter to exit")
